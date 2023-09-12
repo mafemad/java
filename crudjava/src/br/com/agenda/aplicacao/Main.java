5780a19d6402ff038b374a0a2e27883905847f34
mafemad <mafemad2005@gmail.com>
2023-09-12 18:32:02 -0300

    criação metodos read, update e delete

--- crudjava/src/br/com/agenda/aplicacao/Main.java
+++ crudjava/src/br/com/agenda/aplicacao/Main.java
@@ -10,15 +10,36 @@ public class Main {
 		
 			
 		ContatoDAO contatoDao = new ContatoDAO();
-			
+		
+		//criacção de usuario
+		
 		Contato contato = new Contato();
-		contato.setNome("gersin2");
+		contato.setNome("gersin4");
 		contato.setIdade(15);
 		contato.setDataCadastro(new Date());
 			
 		contatoDao.save(contato);
 			
-			
+		//visualização de dados TODOS
+		
+		for(Contato c : contatoDao.getContatos()) {
+			System.out.println("Contato: " + c.getNome());
+			System.out.println("idade: " + c.getIdade());
+			System.out.println("data: " + c.getDataCadastro());
+			System.out.println("-----------------------------");
+		}
+		
+		//atualizar dados 
+		Contato c1 = new Contato();
+		c1.setNome("joazin da massa");
+		c1.setIdade(23);
+		c1.setDataCadastro(new Date());
+		c1.setId(1);//numero do banco de dados
+		
+		contatoDao.update(c1);
+		
+		//deletar contato pelo id
+		contatoDao.deleteById(1);
 		
 	}
 
--- crudjava/src/br/com/agenda/dao/ContatoDAO.java
+++ crudjava/src/br/com/agenda/dao/ContatoDAO.java
@@ -3,10 +3,15 @@ package br.com.agenda.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.agenda.factory.ConnectionFactory;
 import br.com.agenda.contato.Contato;
 
 public class ContatoDAO {
+
 public void save(Contato contato) {
 		
 		String sql = "INSERT INTO contatos(nome, idade, dataCadastro) VALUES (?, ?, ?)";
@@ -53,5 +58,130 @@ public void save(Contato contato) {
 		}
 		
 		
+	}
+
+public List<Contato> getContatos() {
+	
+	String sql = "SELECT * FROM contatos";
+	
+	List<Contato> contatos = new ArrayList<Contato>();
+	
+	Connection conn = null;
+	PreparedStatement pstm = null;
+	//Classe que recupera os dados do banco
+	ResultSet rset = null;
+	
+	try {
+		conn = ConnectionFactory.createConnectionToMySQL();
+		
+		pstm = conn.prepareStatement(sql);
+		
+		rset = pstm.executeQuery();
+		
+		while (rset.next()) {
+			
+			Contato contato = new Contato();
+			
+			//Recuperar o id
+			contato.setId(rset.getInt("id"));
+			//Recuperar o nome 
+			contato.setNome(rset.getString("nome"));
+			//Recuperar a idade
+			contato.setIdade(rset.getInt("idade"));
+			//recuperar data de cadastro
+			contato.setDataCadastro(rset.getDate("datacadastro"));
+			
+			contatos.add(contato);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();			
+		}finally {
+			try {
+				if (rset != null) {
+					rset.close();
+				}
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			return contatos;
+}
+
+public void update (Contato contato) {
+	String sql = "UPDATE contatos SET nome = ?, idade = ?, datacadastro = ?" + "WHERE id = ?";
+	
+	Connection conn = null;
+	PreparedStatement pstm = null;
+	
+	try {
+		//criar conexão com banco 
+		conn = ConnectionFactory.createConnectionToMySQL();
+		
+		//executar a query
+		pstm = conn.prepareStatement(sql);
+		
+		//adicionar valores para atualizar
+		pstm.setString(1, contato.getNome());
+		
+		pstm.setInt(2, contato.getIdade());
+		
+		pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+				
+		pstm.setInt(4, contato.getId());
+		
+		pstm.execute();
+		
+	}catch (Exception e) {
+		e.printStackTrace();
+	}finally {
+		try {
+			if (pstm != null) {
+				pstm.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+public void deleteById(int id) {
+	String sql = "DELETE FROM contatos WHERE id = ?";
+	
+	Connection conn = null;
+	PreparedStatement pstm = null;
+	
+	try {
+		conn = ConnectionFactory.createConnectionToMySQL();
+		
+		pstm = conn.prepareStatement(sql);
+		
+		pstm.setInt(1, id);
+		
+		pstm.execute();
+		
+	}catch (Exception e) {
+		e.printStackTrace();
+	}finally {
+		try {
+			if (pstm != null) {
+				pstm.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	}
 }
